fb83d2e5c570751ab2fb74a7e6fe2d6ed15a67ad
joshua1b <joshua1b2823@gmail.com>
2017-10-01 16:44:44 +0900

    removed functions to get informations about food since it is in package handler.

--- app/model/model.go
+++ app/model/model.go
@@ -1,27 +1,11 @@
 package model
 
 import (
-	"time"
-
-	"database/sql"
-
 	_ "github.com/lib/pq"
 )
 
-func GetDeliciousFoods(db *sql.DB, t *time.Time) ([]string, error) {
-	var delicious_foods []string
-	err := db.QueryRow("SELECT delicious_foods lunch WHERE date = $1", t.Format("20060102")).Scan(&delicious_foods)
-	if err != nil {
-		return []string{}, err
-	}
-	return delicious_foods, nil
+type Food struct {
+	Name string
 }
 
-func GetFoods(db *sql.DB, t *time.Time) ([]string, error) {
-	var foods []string
-	err := db.QueryRow("SELECT foods FROM lunch WHERE date = $1", t.Format("20060102")).Scan(&foods)
-	if err != nil {
-		return []string{}, err
-	}
-	return foods, nil
-}
+type DeliciousFood Food
