a3efc78f03d1b7c4c625c518dc95a189c54ef4bc
joshua1b <joshua1b2823@gmail.com>
2017-11-03 16:28:55 +0900

    Remove EmailConfig since the feature isn't implemeted.

--- config/config.go
+++ config/config.go
@@ -1,8 +1,7 @@
 package config
 
 type Config struct {
-	DB     *DBConfig
-	Emails []*EmailConfig
+	DB *DBConfig
 }
 
 type DBConfig struct {
@@ -10,12 +9,6 @@ type DBConfig struct {
 	Username string
 	Password string
 	Name     string
-}
-
-type EmailConfig struct {
-	From     string
-	To       string
-	Password string
 }
 
 func GetConfig() *Config {
