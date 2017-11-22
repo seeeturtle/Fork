a6695642d6748be5f1a7ad9ad17a390fe1b1e1f0
joshua1b <joshua1b2823@gmail.com>
2017-11-22 23:35:19 +0900

    More accurate similarity.

--- app/handler/parse.go
+++ app/handler/parse.go
@@ -168,11 +168,38 @@ func inIntSlice(a int, b []int) bool {
 
 func separate(a rune) []int {
 	var slice []int
+	var hangulCodes = []string{"ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ",
+		"ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"}
+	if 12592 < int(a) && int(a) < 12687 {
+		var a, b int = -1, -1
+		for i, c := range hangulCodes {
+			if string(a) == c {
+				if inIntSlice(i, []int{1, 4, 8, 10, 13}) {
+					a = i - 1
+					b = i - 1
+				} else {
+					a = i
+				}
+				break
+			}
+		}
+		if a != -1 {
+			if b != -1 {
+				return append(slice, a, b)
+			}
+			return append(slice, a)
+		}
+		return []int{}
+	}
 	code := int(a) - 44032
 	jongSeong := code % 28
 	jungSeong := ((code - jongSeong) / 28) % 21
 	choSeong := ((code-jongSeong)/28 - jungSeong) / 21
-	slice = append(slice, choSeong)
+	if inIntSlice(choSeong, []int{1, 4, 8, 10, 13}) {
+		slice = append(slice, choSeong-1, choSeong-1)
+	} else {
+		slice = append(slice, choSeong)
+	}
 	slice = append(slice, jungSeong)
 	if jongSeong != 0 {
 		slice = append(slice, jongSeong)
