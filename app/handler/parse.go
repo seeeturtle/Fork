8105bc220a124eed1dab010d6840251fb9eabc7c
joshua1b <joshua1b2823@gmail.com>
2018-01-02 23:02:30 +0900

    Extract caculating similarity into new library.

--- app/handler/parse.go
+++ app/handler/parse.go
@@ -4,12 +4,18 @@ import (
 	"regexp"
 	"strings"
 	"time"
+
+	"github.com/seeeturtle/chopstick"
 )
 
 func parseContent(str string) (ok, delicious, similar, slang bool, scope Scope) {
 	splitted := strings.Split(str, " ")
 	re := regexp.MustCompile(`[\d]+월[\d]+일`)
 	scope = NilScope{}
+	exceptions := make([]string, 0)
+	for _, s := range Scopes {
+		exceptions = append(exceptions, s.Name())
+	}
 	for _, w := range splitted {
 		if w == "" {
 			continue
@@ -126,63 +132,63 @@ func parseContent(str string) (ok, delicious, similar, slang bool, scope Scope) 
 					name: "다음달",
 				}
 			}
-		case similarity([]rune("오늘"), []rune(w)) >= 0.5:
+		case chopstick.Similarity([]rune("오늘"), []rune(w)) >= 0.5:
 			if isNilScope {
 				scope = Today{
 					name: "오늘",
 				}
 				similar = true
 			}
-		case similarity([]rune("내일"), []rune(w)) >= 0.5:
+		case chopstick.Similarity([]rune("내일"), []rune(w)) >= 0.5:
 			if isNilScope {
 				scope = Tomorrow{
 					name: "내일",
 				}
 				similar = true
 			}
-		case similarity([]rune("모레"), []rune(w)) >= 0.42:
+		case chopstick.Similarity([]rune("모레"), []rune(w)) >= 0.42:
 			if isNilScope {
 				scope = Nextomorrow{
 					name: "모레",
 				}
 				similar = true
 			}
-		case similarity([]rune("글피"), []rune(w)) >= 0.5:
+		case chopstick.Similarity([]rune("글피"), []rune(w)) >= 0.5:
 			if isNilScope {
 				scope = Threemorrow{
 					name: "글피",
 				}
 				similar = true
 			}
-		case similarity([]rune("이번주"), []rune(w)) >= 0.5:
+		case chopstick.Similarity([]rune("이번주"), []rune(w)) >= 0.5:
 			if isNilScope {
 				scope = ThisWeek{
 					name: "이번주",
 				}
 				similar = true
 			}
-		case similarity([]rune("다음주"), []rune(w)) >= 0.5:
+		case chopstick.Similarity([]rune("다음주"), []rune(w)) >= 0.5:
 			if isNilScope {
 				scope = NextWeek{
 					name: "다음주",
 				}
 				similar = true
 			}
-		case similarity([]rune("다다음주"), []rune(w)) >= 0.5:
+		case chopstick.Similarity([]rune("다다음주"), []rune(w)) >= 0.5:
 			if isNilScope {
 				scope = WeekAfterNext{
 					name: "다다음주",
 				}
 				similar = true
 			}
-		case similarity([]rune("이번달"), []rune(w)) >= 0.5:
+		case chopstick.Similarity([]rune("이번달"), []rune(w)) >= 0.5:
 			if isNilScope {
 				scope = ThisMonth{
 					name: "이번달",
 				}
 				similar = true
 			}
-		case similarity([]rune("다음달"), []rune(w)) >= 0.5:
+		case chopstick.Similarity([]rune("다음달"), []rune(w)) >= 0.5:
 			if isNilScope {
 				scope = NextMonth{
 					name: "다음달",
@@ -199,7 +205,7 @@ func parseContent(str string) (ok, delicious, similar, slang bool, scope Scope) 
 		if strings.Contains(w, "맛있") {
 			delicious = true
 		}
-		if slangSimilarity(w) >= 0.3 {
+		if chopstick.SlangSimilarity(w, exceptions) >= 0.3 {
 			slang = true
 		}
 	}
@@ -207,132 +213,4 @@ func parseContent(str string) (ok, delicious, similar, slang bool, scope Scope) 
 		scope = UndefinedScope{}
 	}
 	return
-}
-
-func similarity(a, b []rune) float64 {
-	intersection := make([]int, 0)
-	union := make([]int, 0)
-	var longer, shorter []int
-	sliceA := make([]int, 0)
-	for _, r := range a {
-		sliceA = append(sliceA, separate(r)...)
-	}
-	sliceA = cutByTwo(sliceA)
-	sliceB := make([]int, 0)
-	for _, r := range b {
-		sliceB = append(sliceB, separate(r)...)
-	}
-	sliceB = cutByTwo(sliceB)
-	if len(sliceA) >= len(sliceB) {
-		longer = sliceA
-		shorter = sliceB
-	} else {
-		longer = sliceB
-		shorter = sliceA
-	}
-	for _, i := range shorter {
-		if !inIntSlice(i, union) {
-			union = append(union, i)
-		}
-		for _, j := range longer {
-			if !inIntSlice(j, union) {
-				union = append(union, j)
-			}
-			if i == j && !inIntSlice(i, intersection) {
-				intersection = append(intersection, i)
-			}
-		}
-	}
-	intersectionLen := len(intersection)
-	unionLen := len(union)
-	return float64(intersectionLen) / float64(unionLen)
-}
-
-func inIntSlice(a int, b []int) bool {
-	for _, i := range b {
-		if a == i {
-			return true
-		}
-	}
-	return false
-}
-
-func separate(a rune) []int {
-	var slice []int
-	var hangulCodes = []string{"ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ",
-		"ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"}
-	if 12592 < int(a) && int(a) < 12687 {
-		var a, b int = -1, -1
-		for i, c := range hangulCodes {
-			if string(a) == c {
-				if inIntSlice(i, []int{1, 4, 8, 10, 13}) {
-					a = i - 1
-					b = i - 1
-				} else {
-					a = i
-				}
-				break
-			}
-		}
-		if a != -1 {
-			if b != -1 {
-				return append(slice, a, b)
-			}
-			return append(slice, a)
-		}
-		return []int{}
-	}
-	code := int(a) - 44032
-	jongSeong := code % 28
-	jungSeong := ((code - jongSeong) / 28) % 21
-	choSeong := ((code-jongSeong)/28 - jungSeong) / 21
-	if inIntSlice(choSeong, []int{1, 4, 8, 10, 13}) {
-		slice = append(slice, choSeong-1, choSeong-1)
-	} else {
-		slice = append(slice, choSeong)
-	}
-	slice = append(slice, jungSeong)
-	if jongSeong != 0 {
-		slice = append(slice, jongSeong)
-	}
-
-	return slice
-}
-
-func cutByTwo(a []int) []int {
-	var result []int
-	if len(a) <= 0 {
-		return result
-	}
-	result = append(result, (a[0]+3)*30)
-	for index, i := range a {
-		if index == len(a)-1 {
-			result = append(result, 100*(i+3))
-			continue
-		}
-		result = append(result, (i+3)*a[index+1])
-	}
-	return result
-}
-
-func slangSimilarity(str string) float64 {
-	var preprocessed string
-	var similaritys []float64
-	re := regexp.MustCompile("[^가-힣]")
-	preprocessed = re.ReplaceAllString(str, "")
-	for _, s := range Scopes {
-		if similarity([]rune(preprocessed), []rune(s.Name())) > 0.4 {
-			return 0
-		}
-	}
-	for _, s := range slangs {
-		similaritys = append(similaritys, similarity([]rune(s), []rune(preprocessed)))
-	}
-	var max float64
-	for _, s := range similaritys {
-		if s >= max {
-			max = s
-		}
-	}
-	return max
 }
